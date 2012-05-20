6db1e9e72228015a622bb9d02bc644f8e22a1366
dr. Hannibal Lecter <lecterror@gmail.com>
2012-05-20 14:17:09 +0200

    baked version

---
+++ Controller/NaiveBayesClassifierAppController.php
@@ -0,0 +1,6 @@
+<?php
+
+class NaiveBayesClassifierAppController extends AppController
+{
+}
+
---
+++ Model/NaiveBayesClassifierAppModel.php
@@ -0,0 +1,7 @@
+<?php
+
+class NaiveBayesClassifierAppModel extends AppModel
+{
+	public $actsAs = array('Containable');
+}
+
